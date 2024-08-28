d34b4039deb21292bcb72420c58b55b7683862a9
duchevich <duchevich274@gmail.com>
2024-08-28 11:17:04 +0200

    service pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
+import Front from './pages/Front'
+import About from './pages/About'
+import Contacts from './pages/Contacts'
+import Terms from './pages/Terms'
+import Privacy from './pages/Privacy'
+import Copyright from './pages/Copyright'
+import NotFound from './pages/NotFound'
 
 const App = () => {
     return (
@@ -7,13 +14,13 @@ const App = () => {
             <div className='App'>
                 <Routes>
                     <Route path='/' element={<MainLayout />}>
-                        <Route path='/' element={<h1>Home</h1>} />
-                        <Route path='about' element={<h1>About</h1>} />
-                        <Route path='contacts' element={<h1>Contacts</h1>} />
-                        <Route path='terms' element={<h1>Terms</h1>} />
-                        <Route path='privacy' element={<h1>Privacy</h1>} />
-                        <Route path='copyright' element={<h1>Copyright</h1>} />
-                        <Route path='*' element={<h1>404</h1>} />
+                        <Route path='/' element={<Front />} />
+                        <Route path='about' element={<About />} />
+                        <Route path='contacts' element={<Contacts />} />
+                        <Route path='terms' element={<Terms />} />
+                        <Route path='privacy' element={<Privacy />} />
+                        <Route path='copyright' element={<Copyright />} />
+                        <Route path='*' element={<NotFound />} />
                     </Route>
                 </Routes>
             </div>
---
+++ src/pages/Copyright.jsx
@@ -0,0 +1,8 @@
+const Copyright = () => {
+    return (
+        <div>
+            <h1>Copyright</h1>
+        </div>
+    )
+}
+export default Copyright
